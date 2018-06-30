a3fe557efcea8945f02993fc8571993d6671d9fe
Ryan1729 <Ryan1729@gmail.com>
2018-06-30 00:15:31 -0600

    fixed expression form candrop

--- src/game.rs
+++ src/game.rs
@@ -326,41 +326,34 @@ fn candrop(cells: &Cells, grabpos: u8, grabdepth: u8, droppos: u8) -> bool {
     };
 
     if droppos < BUTTON_COLUMN {
-        return cells[droppos as usize].len() == 0 && grabdepth == 0;
+        cells[droppos as usize].len() == 0 && grabdepth == 0
     } else if droppos >= BUTTON_COLUMN && droppos <= FLOWER_FOUNDATION {
-        return false;
+        false
     } else if droppos >= START_OF_FOUNDATIONS && droppos < START_OF_TABLEAU {
         let droppos = droppos as usize;
         if grabdepth == 0 {
             if cells[droppos].len() == 0 {
-                if getcardnum(grabcard) == 1 {
-                    return true;
-                }
+                getcardnum(grabcard) == 1
             } else {
                 let dropcard = last_unchecked!(cells[droppos]);
-                if getsuit(grabcard) == getsuit(dropcard)
+
+                getsuit(grabcard) == getsuit(dropcard)
                     && getcardnum(grabcard) != 0
                     && getcardnum(grabcard) == getcardnum(dropcard) + 1
-                {
-                    return true;
-                }
             }
+        } else {
+            false
         }
-        return false;
     } else {
         let droppos = droppos as usize;
         if cells[droppos].len() == 0 {
-            return true;
+            true
         } else {
             let dropcard = last_unchecked!(cells[droppos]);
-            if getsuit(grabcard) != getsuit(dropcard)
+            getsuit(grabcard) != getsuit(dropcard)
                 && getcardnum(grabcard) != 0
                 && getcardnum(grabcard) == getcardnum(dropcard) - 1
-            {
-                return true;
-            }
         }
-        return false;
     }
 }
 
